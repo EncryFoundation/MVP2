2a4f0b069aeeff4fcfd023392f6a8bbe1dc55002
timofey <gusev.t.s1@gmail.com>
2018-10-24 10:38:03 +0300

    added logging system

--- src/main/scala/Actors/TestActor.scala
+++ src/main/scala/Actors/TestActor.scala
@@ -1,17 +1,21 @@
 package Actors
 
 import akka.actor.Actor
+import com.typesafe.scalalogging.StrictLogging
 
-class TestActor extends Actor{
+class TestActor extends Actor with StrictLogging {
 
   import Actors.TestActor.Message
 
   override def receive: Receive = {
     case message: Message => println(message.info)
+      logger.info(message.info)
   }
 
 }
 
 object TestActor {
-  case class Message (info: String)
+
+  case class Message(info: String)
+
 }
--- src/main/scala/MVP2.scala
+++ src/main/scala/MVP2.scala
@@ -2,10 +2,12 @@ import Actors.TestActor
 import Actors.TestActor.Message
 import akka.actor.{ActorSystem, Props}
 import akka.stream.ActorMaterializer
+import com.typesafe.scalalogging.StrictLogging
 import scala.concurrent.ExecutionContextExecutor
 
-object MVP2 extends App {
+object MVP2 extends App with StrictLogging {
   println("Hello, world!")
+  logger.info("Hello, world!")
 
   implicit val system: ActorSystem = ActorSystem()
   implicit val materializer: ActorMaterializer = ActorMaterializer()
