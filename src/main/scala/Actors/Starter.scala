ea93d682f0cdfda8e13b4ed3218294c45562715b
Stanislav Kapinus <kapinuss@rambler.ru>
2018-10-24 11:45:33 +0300

    starter

--- src/main/scala/Actors/Starter.scala
+++ src/main/scala/Actors/Starter.scala
@@ -1,10 +1,15 @@
 package Actors
 
 import akka.actor.{Actor, Props}
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.language.postfixOps
 
 class Starter extends Actor {
 
   import Messages.{Start, InfoMessage}
+
+  context.system.scheduler.schedule(0 seconds, 3 seconds, self, InfoMessage("Self ping by sheduler."))
 
   override def preStart(): Unit = {
     println("Starting the Starter!")
