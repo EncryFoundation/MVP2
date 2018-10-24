bcaecde88e7cb0903dfc1edb2243003a46cd9b0c
aleksandr <alexbromel@gmail.com>
2018-10-24 12:15:20 +0300

    Dome additions

--- src/main/scala/Actors/Sender.scala
+++ src/main/scala/Actors/Sender.scala
@@ -11,7 +11,7 @@ import scala.concurrent.duration._
 
 class Sender extends Actor with StrictLogging {
 
-  val remote = new InetSocketAddress("localhost", 1234)
+  val remote: InetSocketAddress = new InetSocketAddress("localhost", 1234)
 
   override def preStart(): Unit = {
     context.system.scheduler.schedule(1.seconds, 1.seconds)(self ! Ping)
