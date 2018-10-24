a428385ef4b701709bae2b5e75801f3c69680709
aleksandr <alexbromel@gmail.com>
2018-10-24 12:13:07 +0300

    logger.info -> logger.debug

--- src/main/scala/Actors/Receiver.scala
+++ src/main/scala/Actors/Receiver.scala
@@ -12,12 +12,12 @@ class Receiver extends Actor with StrictLogging {
   import context.system
 
   override def preStart(): Unit = {
-    IO(Udp) ! Udp.Bind(self, new InetSocketAddress("localhost", 1234))
+    IO(Udp) ! Udp.Bind(self, new InetSocketAddress("localhost", 5678))
   }
 
   override def receive: Receive = {
     case Udp.Bound(local) =>
-      logger.info(s"Binded to $local")
+      logger.debug(s"Binded to $local")
       context.become(readCycle(sender))
       context.actorSelection("/user/Sender/") ! UdpSocket(sender)
     case msg => logger.warn(s"Received message $msg from $sender before binding")
@@ -25,19 +25,19 @@ class Receiver extends Actor with StrictLogging {
 
   def readCycle(socket: ActorRef): Receive = {
     case Udp.Received(data: ByteString, remote) =>
-      logger.info(s"Received ${data.utf8String} from $remote")
+      logger.debug(s"Received ${data.utf8String} from $remote")
       data.utf8String match {
         case "Ping" =>
-          logger.info(s"Get ping from: $remote send Pong")
+          logger.debug(s"Get ping from: $remote send Pong")
           context.actorSelection("/user/Sender") ! Pong
         case "Pong" =>
-          logger.info(s"Get pong from: $remote send Pong")
+          logger.debug(s"Get pong from: $remote send Pong")
       }
     case Udp.Unbind  =>
       socket ! Udp.Unbind
-      logger.info(s"Unbind $socket")
+      logger.debug(s"Unbind $socket")
     case Udp.Unbound =>
-      logger.info(s"Unbound $socket")
+      logger.debug(s"Unbound $socket")
       context.stop(self)
   }
 }
--- src/main/scala/Actors/Sender.scala
+++ src/main/scala/Actors/Sender.scala
@@ -11,7 +11,7 @@ import scala.concurrent.duration._
 
 class Sender extends Actor with StrictLogging {
 
-  val remote = new InetSocketAddress("localhost", 5678)
+  val remote = new InetSocketAddress("localhost", 1234)
 
   override def preStart(): Unit = {
     context.system.scheduler.schedule(1.seconds, 1.seconds)(self ! Ping)
@@ -19,16 +19,16 @@ class Sender extends Actor with StrictLogging {
 
   override def receive: Receive = {
     case UdpSocket(connection) => context.become(sendingCycle(connection))
-    case msg => logger.info(s"Smth strange: $msg")
+    case msg => logger.debug(s"Smth strange: $msg")
   }
 
   def sendingCycle(connection: ActorRef): Receive = {
     case Ping =>
       connection ! Udp.Send(ByteString("Ping"), remote)
-      logger.info(s"Send ping to: $connection")
+      logger.debug(s"Send ping to: $connection")
     case Pong =>
       connection ! Udp.Send(ByteString("Pong"), remote)
-      logger.info(s"Send pong to remote: $connection")
+      logger.debug(s"Send pong to remote: $connection")
   }
 }
 
