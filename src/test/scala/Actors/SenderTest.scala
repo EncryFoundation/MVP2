a023f07deb844569a2e1a5f42b5bcbbec4e9d45c
aleksandr <alexbromel@gmail.com>
2018-10-24 13:11:40 +0300

    Cleanup

--- src/test/scala/Actors/SenderTest.scala
+++ src/test/scala/Actors/SenderTest.scala
@@ -1,8 +1,7 @@
 package Actors
 
 import java.net.InetSocketAddress
-
-import Actors.Sender.{Ping, UdpSocket}
+import Actors.Sender.{Ping, Pong, UdpSocket}
 import akka.actor.{ActorSystem, Props}
 import akka.io.Udp
 import akka.testkit.{ImplicitSender, TestKit, TestProbe}
@@ -12,19 +11,18 @@ import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
 class SenderTest extends TestKit(ActorSystem("SenderTestSystem")) with ImplicitSender
   with FlatSpecLike with Matchers with BeforeAndAfterAll {
 
+  val probe = TestProbe()
   val sender = system.actorOf(Props[Sender], "Sender")
 
   it should "send ping message to udp.socket when receive case object Ping" in {
-    val probe = TestProbe()
     sender ! UdpSocket(probe.ref)
     sender ! Ping
     probe.expectMsg(Udp.Send(ByteString("Ping"), new InetSocketAddress("localhost", 1234)))
   }
 
   it should "send pong message to udp.socket when receive case object Pong" in {
-    val probe = TestProbe()
     sender ! UdpSocket(probe.ref)
-    sender ! Ping
+    sender ! Pong
     probe.expectMsg(Udp.Send(ByteString("Pong"), new InetSocketAddress("localhost", 1234)))
   }
 }
