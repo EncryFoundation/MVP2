17e3b8ed1fede96e351e72e54cb7363867c1fcba
Stanislav Kapinus <kapinuss@rambler.ru>
2018-10-24 10:20:22 +0300

    actors

--- src/main/scala/MVP2.scala
+++ src/main/scala/MVP2.scala
@@ -1,4 +1,6 @@
-import akka.actor.ActorSystem
+import Actors.TestActor
+import Actors.TestActor.Message
+import akka.actor.{ActorSystem, Props}
 import akka.stream.ActorMaterializer
 import scala.concurrent.ExecutionContextExecutor
 
@@ -9,6 +11,7 @@ object MVP2 extends App {
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   implicit val ec: ExecutionContextExecutor = system.dispatcher
 
-
+  system.actorOf(Props[TestActor], "testActor")
+  system.actorSelection("/user/testActor") ! Message("Hello, actor!")
 
 }
