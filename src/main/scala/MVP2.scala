93199fdfeed2f7bc96bd598d5e82ad7a9bc9d1e2
Stanislav Kapinus <kapinuss@rambler.ru>
2018-10-24 10:13:26 +0300

    sbt

--- src/main/scala/MVP2.scala
+++ src/main/scala/MVP2.scala
@@ -1,3 +1,14 @@
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import scala.concurrent.ExecutionContextExecutor
+
 object MVP2 extends App {
   println("Hello, world!")
+
+  implicit val system: ActorSystem = ActorSystem()
+  implicit val materializer: ActorMaterializer = ActorMaterializer()
+  implicit val ec: ExecutionContextExecutor = system.dispatcher
+
+
+
 }
