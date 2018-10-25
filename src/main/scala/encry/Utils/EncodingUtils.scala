480138e85f7d980c4a392f6f60c41fa4f250189b
timofey <gusev.t.s1@gmail.com>
2018-10-25 14:09:59 +0300

    removed Base16

--- src/main/scala/encry/Utils/EncodingUtils.scala
+++ src/main/scala/encry/Utils/EncodingUtils.scala
@@ -1,12 +1,10 @@
 package encry.Utils
 
 import akka.util.ByteString
-import encry.Utils.Base16.encode
 import io.circe.{Decoder, Encoder}
 import io.circe.syntax._
 
 object EncodingUtils {
-  implicit val byteStringEncoder: Encoder[ByteString] = bytes => encode(bytes).asJson
-  implicit val byteStringDecoder: Decoder[ByteString] =
-    Decoder.decodeString.map(Base16.decode).map(_.getOrElse(ByteString.empty))
+  implicit val byteStringEncoder: Encoder[ByteString] = _.utf8String.asJson
+  implicit val byteStringDecoder: Decoder[ByteString] = Decoder.decodeString.map(str => ByteString(str))
 }
