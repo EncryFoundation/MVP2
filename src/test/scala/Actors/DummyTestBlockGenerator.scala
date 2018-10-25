0de7e8d6c3bb754e09c4b1926eec19c2156ab78a
ugulavaGeorge <ugulava.george@gmail.com>
2018-10-25 18:59:23 +0300

    test sceletons

---
+++ src/test/scala/Actors/DummyTestBlockGenerator.scala
@@ -0,0 +1,28 @@
+package Actors
+
+import java.nio.charset.StandardCharsets
+
+import Data.{Block, GeneralBlock, MicroBlock, Transaction}
+import akka.util.ByteString
+import utils.Sha256
+
+object DummyTestBlockGenerator {
+
+  var addressNoncePull: Map[Int, (ByteString, Int)] = List.range(1,10).map(_ => generateByteString)
+    .zip(List.fill(10)(0)).zipWithIndex.map(_.swap).toMap
+
+  def generateKeyBlock(prevHash: ByteString, prevHeight: Int): GeneralBlock =
+    GeneralBlock(prevHeight+1, prevHash, generateTenTransactions, ByteString.empty)
+
+  def generateMicroBlock(prevHash: ByteString, prevMicroHash: ByteString, prevHeight: Int): MicroBlock =
+    MicroBlock(prevHeight+1, prevHash, prevMicroHash, generateTenTransactions, ByteString.empty)
+
+  def generateTenTransactions: List[Transaction] = List.range(1,10).map(_ => generateTransaction)
+  def generateTransaction: Transaction = Transaction()
+  def generateChain(length:Int): List[Block] = List.range(1,10)
+    .map(e => generateKeyBlock(generateByteString,e))
+
+
+  def generateByteString: ByteString = ByteString(java.util.UUID.randomUUID().toString.getBytes(StandardCharsets.UTF_8))
+  def generateHash: ByteString = Sha256.toSha256(java.util.UUID.randomUUID().toString)
+}
---
+++ src/test/scala/Actors/StateSpec.scala
@@ -0,0 +1,9 @@
+package Actors
+
+import akka.actor.ActorSystem
+import akka.testkit.TestKit
+import org.scalatest.Matchers
+
+class StateSpec extends TestKit(ActorSystem("StateTestSystem")) with Matchers{
+
+}
