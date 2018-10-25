f22952339e663de7cca2c507a064e0e92f7c1598
timofey <gusev.t.s1@gmail.com>
2018-10-25 14:21:29 +0300

    code cleanup

--- src/main/scala/encry/http/HttpServer.scala
+++ src/main/scala/encry/http/HttpServer.scala
@@ -5,9 +5,6 @@ import encry.MVP2._
 import encry.Utils.Settings
 
 object HttpServer {
+  def start(): Unit = Http().bindAndHandle(ApiRoute().apiInfo, Settings.settings.httpHost, Settings.settings.httpPort)
 
-  def start(): Unit = {
-    val (host, port): (String, Int) = (Settings.settings.httpHost, Settings.settings.httpPort)
-    Http().bindAndHandle(ApiRoute().apiInfo, host, port)
-  }
 }
