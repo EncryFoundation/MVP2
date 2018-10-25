62af08d469a19c5c63dde3ae6932cefa90ec0309
ugulavaGeorge <ugulava.george@gmail.com>
2018-10-25 15:13:31 +0300

    state refactoring, adt removed

--- src/main/scala/Actors/Accountant.scala
+++ src/main/scala/Actors/Accountant.scala
@@ -1,12 +1,8 @@
 package Actors
 
 import Data.{GeneralBlock, MicroBlock, Transaction}
-import akka.util.ByteString
-import scala.collection.immutable.HashMap
 
 class Accountant extends CommonActor {
-
-  var currentState: State = GenesisState(HashMap.empty)
 
   override def specialBehavior: Receive = {
     case microBlock: MicroBlock =>
@@ -17,20 +13,8 @@ class Accountant extends CommonActor {
 
   def updateState(transactions: List[Transaction]): Unit = {
     transactions.groupBy(_.publicKey).foreach {
-      singleParty =>
-        currentState match {
-          case fs: FunctioningState => updateState(singleParty._1, fs.accountsInfo, singleParty._2)
-          case gs: GenesisState => updateState(singleParty._1, gs.accountsInfo, singleParty._2)
-        }
+      singleParty => State.updateState(singleParty._1, singleParty._2)
     }
-  }
-
-  def updateState(pubKey: ByteString, accounts: HashMap[ByteString, Account], transactions: List[Transaction]): Unit = {
-    var account: Account = accounts.getOrElse(pubKey, Account(pubKey, List.empty,0))
-    transactions.sortBy(_.nonce).foreach { tx =>
-      if (account.nonce + 1 == tx.nonce)
-        account = account.copy(data = account.data :+ tx.data.getOrElse(ByteString.empty), nonce = tx.nonce)
-    }
-    currentState = FunctioningState(accounts + (pubKey -> account))
   }
 }
+
--- src/main/scala/Actors/State.scala
+++ src/main/scala/Actors/State.scala
@@ -1,13 +1,27 @@
 package Actors
 
+import Data.Transaction
 import akka.util.ByteString
-
+import utils.Sha256
 import scala.collection.immutable.HashMap
 
-sealed trait State {}
+object State {
+  private var accountsInfo: HashMap[ByteString, Account] = HashMap.empty
+  private var stateRoot: ByteString = Sha256.toSha256(accountsInfo.toString)
 
-case class GenesisState(accountsInfo: HashMap[ByteString, Account]) extends State
+  def updateState(pubKey: ByteString, transactions: List[Transaction]): Unit = {
+    var account: Account = accountsInfo.getOrElse(pubKey, Account(pubKey, List.empty, 0))
+    transactions.sortBy(_.nonce).foreach { tx =>
+      if (account.nonce + 1 == tx.nonce)
+        account = account.copy(data = account.data :+ tx.data.getOrElse(ByteString.empty), nonce = tx.nonce)
+    }
+    accountsInfo = accountsInfo + (pubKey -> account)
+    stateRoot = Sha256.toSha256(accountsInfo.toString)
+  }
 
-case class FunctioningState(accountsInfo: HashMap[ByteString, Account]) extends State
+  def getStateRoot: ByteString = stateRoot
+
+  def getAccountsInfo: HashMap[ByteString, Account] = accountsInfo
+}
 
 case class Account(publicKey: ByteString, data: List[ByteString], nonce: Long)
