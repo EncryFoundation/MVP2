5e0328b089493efedc21a30ea4baef66fca02ef1
Stanislav Kapinus <kapinuss@rambler.ru>
2018-10-31 17:12:35 +0300

    refactoring

--- src/main/scala/mvp2/actors/Publisher.scala
+++ src/main/scala/mvp2/actors/Publisher.scala
@@ -5,7 +5,7 @@ import akka.util.ByteString
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 import mvp2.data.{KeyBlock, Transaction}
-import mvp2.utils.{ECDSA, Sha256}
+import mvp2.utils.ECDSA
 import scala.util.Random
 
 class Publisher extends CommonActor {
@@ -16,11 +16,15 @@ class Publisher extends CommonActor {
 
   context.system.scheduler.schedule(1 second, 5 seconds)(createKeyBlock)
 
-  context.system.scheduler.schedule(1 second, 3 seconds){
+  context.system.scheduler.schedule(1 second, 3 seconds) {
     val randomData: ByteString = ByteString(randomizer.nextString(100))
     val pairOfKeys: KeyPair = ECDSA.createKeyPair
-    val signature = ECDSA.sign(pairOfKeys.getPrivate, randomData)
-    self ! Transaction(ByteString(randomizer.nextString(100)), randomizer.nextLong(), signature,randomData)
+    val signature: ByteString = ECDSA.sign(pairOfKeys.getPrivate, randomData)
+    self ! Transaction(ByteString(pairOfKeys.getPublic.toString), randomizer.nextLong(), signature, randomData)
+  }
+
+  context.system.scheduler.schedule(1 second, 10 seconds) {
+    println(s"There are ${mempool.size} transactions in the mempool.")
   }
 
   override def preStart(): Unit = {
@@ -38,8 +42,8 @@ class Publisher extends CommonActor {
   def createKeyBlock: KeyBlock = {
     val thisBlocksHeight: Long = lastKeyBlock.height + 1
     val currentTime: Long = System.currentTimeMillis
-    val text: String = thisBlocksHeight.toString + currentTime.toString + lastKeyBlock.previousGeneralBlock.toString
-    val keyBlock: KeyBlock = KeyBlock(thisBlocksHeight, currentTime, Sha256.toSha256(text), mempool)
+    val keyBlock: KeyBlock =
+      KeyBlock(thisBlocksHeight, currentTime, lastKeyBlock.currentBlockHash, mempool)
     mempool = List.empty
     println(keyBlock)
     self ! keyBlock
--- src/main/scala/mvp2/data/Block.scala
+++ src/main/scala/mvp2/data/Block.scala
@@ -1,27 +1,45 @@
 package mvp2.data
 
 import akka.util.ByteString
+import mvp2.utils.Sha256
 
 sealed trait Block {
   val height: Long
   val timestamp: Long
+  val previousKeyBlockHash: ByteString
+  val currentBlockHash: ByteString
+
   def isValid: Boolean
 
-  override def toString: String = s"Height: $height, time = $timestamp."
+  override def toString: String = s"Height: $height, time = $timestamp, " +
+    s"previousKeyBlockHash = ${previousKeyBlockHash.utf8String}, " + s"currentBlockHash = ${currentBlockHash.utf8String}."
 }
 
-final case class KeyBlock(height: Long = 0,
-                          timestamp: Long = System.currentTimeMillis,
-                          previousGeneralBlock: ByteString = ByteString.empty,
-                          transactions: List[Transaction] = List.empty,
-                          data: ByteString = ByteString.empty) extends Block {
+final case class KeyBlock(height: Long,
+                          timestamp: Long,
+                          previousKeyBlockHash: ByteString,
+                          currentBlockHash: ByteString,
+                          transactions: List[Transaction],
+                          data: ByteString) extends Block {
   override def isValid: Boolean = ???
+}
+
+object KeyBlock {
+  def apply(height: Long = 0,
+            timestamp: Long = System.currentTimeMillis,
+            previousKeyBlockHash: ByteString = ByteString.empty,
+            transactions: List[Transaction] = List.empty,
+            data: ByteString = ByteString.empty): KeyBlock = {
+    val currentBlockHash = Sha256.toSha256(height.toString + timestamp.toString + previousKeyBlockHash.toString)
+    new KeyBlock(height, timestamp, previousKeyBlockHash, currentBlockHash, transactions, data)
+  }
 }
 
 final case class MicroBlock(height: Long,
                             timestamp: Long,
-                            previousGeneralBlock: ByteString,
-                            previousMiniBlock: ByteString,
+                            previousKeyBlockHash: ByteString = ByteString.empty,
+                            previousMicroBlock: ByteString,
+                            currentBlockHash: ByteString,
                             transactions: List[Transaction],
                             data: ByteString) extends Block {
   override def isValid: Boolean = ???
