5a88decea0ac45a098784db8f65e4ccecf91c011
Stanislav Kapinus <kapinuss@rambler.ru>
2018-11-06 15:29:55 +0300

    refactoring

--- src/main/scala/mvp2/actors/Publisher.scala
+++ src/main/scala/mvp2/actors/Publisher.scala
@@ -1,31 +1,23 @@
 package mvp2.actors
 
-import java.security.KeyPair
-import akka.util.ByteString
+import akka.actor.{ActorRef, Props}
 import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 import mvp2.data.{KeyBlock, Transaction}
-import mvp2.utils.ECDSA
 import scala.language.postfixOps
-import scala.util.Random
 
 class Publisher extends CommonActor {
 
   var mempool: List[Transaction] = List.empty
   var lastKeyBlock: KeyBlock = KeyBlock()
-  val randomizer: Random.type = scala.util.Random
+  val testTxGenerator: ActorRef = context.actorOf(Props(classOf[TestTxGenerator]), "testTxGenerator")
 
   context.system.scheduler.schedule(10 second, 5 seconds)(createKeyBlock)
 
-  context.system.scheduler.schedule(1 second, 3 seconds) {
-    val randomData: ByteString = ByteString(randomizer.nextString(100))
-    val pairOfKeys: KeyPair = ECDSA.createKeyPair
-    val signature: ByteString = ECDSA.sign(pairOfKeys.getPrivate, randomData)
-    self ! Transaction(ByteString(pairOfKeys.getPublic.toString), randomizer.nextLong(), signature, randomData)
-  }
-
   override def specialBehavior: Receive = {
-    case transaction: Transaction => mempool = transaction :: mempool
+    case transaction: Transaction =>
+      logger.info(s"Publisher received tx: $transaction.")
+      mempool = transaction :: mempool
     case keyBlock: KeyBlock => lastKeyBlock = keyBlock
   }
 
