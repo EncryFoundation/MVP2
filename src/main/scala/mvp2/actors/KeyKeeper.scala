5475860f25fe70256eede001ae9106ca12c1cb6f
aleksandr <alexbromel@gmail.com>
2018-11-07 18:53:34 +0300

    Add compress methods to ECDSA

--- src/main/scala/mvp2/actors/KeyKeeper.scala
+++ src/main/scala/mvp2/actors/KeyKeeper.scala
@@ -1,12 +1,15 @@
 package mvp2.actors
 
 import java.security.KeyPair
+import akka.util.ByteString
 import mvp2.messages.Get
 import mvp2.utils.ECDSA
 
 class KeyKeeper extends CommonActor {
 
   val myKeys: KeyPair = ECDSA.createKeyPair
+
+  var nodesKeys: Set[ByteString] = Set.empty
 
   override def specialBehavior: Receive = {
     case Get => sender ! myKeys.getPublic
