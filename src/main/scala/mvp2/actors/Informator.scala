bfbff78de89584640a77d7460fbcb07542419df6
Timofey <gusev.t.s1@gmail.com>
2018-11-09 10:30:48 +0300

    mvp2 - route for txs (#32)
    * added tx route
    
    * fixed
    
    * added post route for a list of transactions
    
    * cleanup code
    
    * added test
    
    * code cleanup

--- src/main/scala/mvp2/actors/Informator.scala
+++ src/main/scala/mvp2/actors/Informator.scala
@@ -2,7 +2,7 @@ package mvp2.actors
 
 import akka.actor.ActorRefFactory
 import mvp2.messages.Get
-import mvp2.http.ApiRoute
+import mvp2.http.Routes
 import mvp2.messages.CurrentBlockchainInfo
 import akka.http.scaladsl.Http
 import mvp2.MVP2._
@@ -27,6 +27,8 @@ class Informator(settings: Settings) extends CommonActor {
 object Informator {
 
   def start(settings: Settings, context: ActorRefFactory): Unit = Http().bindAndHandle(
-    ApiRoute(settings, context).apiInfo, settings.apiSettings.httpHost, settings.apiSettings.httpPort
+    Routes(settings, context).route,
+    settings.apiSettings.httpHost,
+    settings.apiSettings.httpPort
   )
 }
