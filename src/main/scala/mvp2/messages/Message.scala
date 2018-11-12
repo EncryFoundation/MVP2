89778ff1d963739e82def18638b5b2ab86f8708b
aleksandr <alexbromel@gmail.com>
2018-11-12 16:58:48 +0300

    change to trait "Block"

--- src/main/scala/mvp2/messages/Message.scala
+++ src/main/scala/mvp2/messages/Message.scala
@@ -4,7 +4,7 @@ import java.net.InetSocketAddress
 import java.security.PublicKey
 import akka.actor.ActorRef
 import akka.util.ByteString
-import mvp2.data.KeyBlock
+import mvp2.data.{Block, KeyBlock}
 
 sealed trait Message
 
@@ -45,7 +45,7 @@ case object Peers {
   val typeId: Byte = 3: Byte
 }
 
-case class Blocks(blocks: List[KeyBlock]) extends NetworkMessage
+case class Blocks(blocks: List[Block]) extends NetworkMessage
 
 object Blocks {
 
