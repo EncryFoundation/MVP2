c14c59ec2504e2e72d2ce19374de8ba019b07d29
timofey <gusev.t.s1@gmail.com>
2018-11-12 14:28:52 +0300

    code cleanup

--- src/main/scala/mvp2/data/Block.scala
+++ src/main/scala/mvp2/data/Block.scala
@@ -55,10 +55,10 @@ final case class MicroBlock(height: Long,
   override def isValid(previousBlock: Block): Boolean = true
 }
 
-case class LightKeyBlock(override val height: Long = 0,
-                         override val timestamp: Long = 0,
-                         override val previousKeyBlockHash: ByteString = ByteString.empty,
-                         override val currentBlockHash: ByteString = ByteString.empty,
+case class LightKeyBlock(height: Long = 0,
+                         timestamp: Long = 0,
+                         previousKeyBlockHash: ByteString = ByteString.empty,
+                         currentBlockHash: ByteString = ByteString.empty,
                          txNum: Int = 0,
                          data: ByteString = ByteString.empty) extends Block {
   override def isValid(previousBlock: Block): Boolean = true
