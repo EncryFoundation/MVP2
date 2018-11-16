f7fe7da4da53c458b80c57462fb5130aaf3941bc
aleksandr <alexbromel@gmail.com>
2018-11-16 18:02:28 +0300

    Some fix

--- src/main/scala/mvp2/utils/EncodingUtils.scala
+++ src/main/scala/mvp2/utils/EncodingUtils.scala
@@ -6,8 +6,8 @@ import io.circe.{Decoder, Encoder}
 import io.circe.syntax._
 
 object EncodingUtils {
-  implicit val byteStringEncoder: Encoder[ByteString] = _.utf8String.asJson
-  implicit val byteStringDecoder: Decoder[ByteString] = Decoder.decodeString.map(str => ByteString(str))
+  implicit val byteStringEncoder: Encoder[ByteString] = str => encode2Base16(str).asJson
+  implicit val byteStringDecoder: Decoder[ByteString] = Decoder.decodeString.map(decodeFromBase16)
 
   def encode2Base16(bytes: ByteString): String = BaseEncoding.base16().encode(bytes.toArray)
 
