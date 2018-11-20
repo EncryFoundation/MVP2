a001d67baaf98a08a454e90d160c598f0b9dd5fa
aleksandr <alexbromel@gmail.com>
2018-11-20 15:39:41 +0300

    Some additions

--- src/main/scala/mvp2/actors/UdpSender.scala
+++ src/main/scala/mvp2/actors/UdpSender.scala
@@ -6,7 +6,6 @@ import akka.serialization.{Serialization, SerializationExtension}
 import akka.util.ByteString
 import com.typesafe.scalalogging.StrictLogging
 import mvp2.data.InnerMessages.{MsgToNetwork, SendToNetwork, UdpSocket}
-import mvp2.data.NetworkMessages._
 import io.circe.syntax._
 import io.circe.generic.auto._
 import mvp2.utils.EncodingUtils._
@@ -33,15 +32,4 @@ class UdpSender(settings: Settings) extends Actor with StrictLogging {
           remote
         )
   }
-//
-//  def serialize(message: NetworkMessage): ByteString = ByteString(message match {
-//    case peers: Peers => NetworkMessagesId.PeersId +: serialization.findSerializerFor(Peers).toBinary(peers)
-//    case blocks: Blocks => NetworkMessagesId.BlocksId +: serialization.findSerializerFor(blocks).toBinary(blocks)
-//    case syncIterators: SyncMessageIterators =>
-//      NetworkMessagesId.SyncMessageIteratorsId +: serialization.findSerializerFor(syncIterators).toBinary(syncIterators)
-//    case lastBlockHeight: LastBlockHeight =>
-//      NetworkMessagesId.LastBlockHeightId +: serialization.findSerializerFor(lastBlockHeight).toBinary(lastBlockHeight)
-//    case transactions: Transactions =>
-//      NetworkMessagesId.TransactionsId +: serialization.findSerializerFor(transactions).toBinary(transactions)
-//  })
 }
