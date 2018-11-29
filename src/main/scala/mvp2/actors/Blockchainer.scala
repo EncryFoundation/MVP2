a6664900a03df0b6fef0fef1d8359b84310eabb4
timofey <gusev.t.s1@gmail.com>
2018-11-29 18:11:22 +0300

    added logic for syncing epoch

--- src/main/scala/mvp2/actors/Blockchainer.scala
+++ src/main/scala/mvp2/actors/Blockchainer.scala
@@ -86,6 +86,7 @@ class Blockchainer(settings: Settings) extends PersistentActor with StrictLoggin
       )
       logger.info(s"Blockchainer apply new keyBlock with height ${block.height}. " +
         s"Blockchain's height is ${blockchain.chain.size}.")
+
       if (isSynced) publisher ! block
       if (!isSynced && blockchain.isSynced(settings.blockPeriod)) {
         isSynced = true
